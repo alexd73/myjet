03cc28bd7b303160b341d224d740b36fae2ca7c2
alexey <alexd73@gmail.com>
2016-06-08 22:43:56 +0300

    v0.0.0.0.01

--- myjet.js
+++ myjet.js
@@ -1,1 +1,47 @@
-
+"use strict";
+var btn = (function () {
+    function btn(iBtn) {
+        this.button = iBtn;
+    }
+    btn.prototype.render = function () {
+        var ulButtons = document.createElement('ul');
+        ulButtons.innerHTML =
+            "<li>\n                <a class=\"myjet_" + this.button.name + "\" href=\"" + this.button.link + "\">\n                    <i class=\"fa fa-2x fa-pull-left  " + this.button.icon + "\"></i>\n                    <span>" + this.button.label + "</span>\n                </a>\n             </li>";
+        return ulButtons;
+    };
+    return btn;
+}());
+var myJet = (function () {
+    function myJet() {
+        this.opt = {
+            buttons: [
+                {
+                    name: 'calc',
+                    label: 'Рассчитать доставку',
+                    link: '#myjet_calc',
+                    icon: 'fa-truck'
+                },
+                {
+                    name: 'order',
+                    label: 'Сделать заказ',
+                    link: '#myjet_order',
+                    icon: 'fa-phone'
+                }
+            ]
+        };
+    }
+    myJet.prototype.render = function () {
+        var divMyjet = document.createElement('div');
+        divMyjet.id = 'myjet';
+        divMyjet.className = 'left bottom roundcorner';
+        this.opt.buttons.forEach(function (ibtn, index) {
+            var but = new btn(ibtn);
+            divMyjet.appendChild(but.render());
+        });
+        return divMyjet;
+    };
+    return myJet;
+}());
+var myjet = new myJet();
+document.body.appendChild(myjet.render());
+//# sourceMappingURL=myjet.js.map
---
+++ myjet.ts
@@ -0,0 +1,79 @@
+
+interface iElement {
+    render(): HTMLElement;
+}
+
+/**
+ * Просто описывает поля кнопки
+ */
+export interface iButton {
+    name: string;
+    label: string;
+    link: string;
+    icon: string;
+}
+
+interface iOptions {
+    buttons: iButton[];
+}
+
+class btn implements iElement {
+    button: iButton;
+
+    constructor (iBtn: iButton) {
+        this.button = iBtn;
+    }
+
+    render(){
+        let ulButtons = document.createElement('ul');
+        ulButtons.innerHTML =
+            `<li>
+                <a class="myjet_${this.button.name}" href="${this.button.link}">
+                    <i class="fa fa-2x fa-pull-left  ${this.button.icon}"></i>
+                    <span>${this.button.label}</span>
+                </a>
+             </li>`;
+        return ulButtons;
+    }
+}
+
+
+
+class myJet implements iElement {
+    element: iElement[];
+    opt: iOptions;
+
+    constructor () {
+        this.opt = {
+            buttons: [
+                {
+                    name: 'calc',
+                    label: 'Рассчитать доставку',
+                    link: '#myjet_calc',
+                    icon: 'fa-truck'
+                },
+                {
+                    name: 'order',
+                    label: 'Сделать заказ',
+                    link: '#myjet_order',
+                    icon: 'fa-phone'
+                }
+
+            ]
+        };
+    }
+
+    render(){
+        let divMyjet = document.createElement('div');
+        divMyjet.id = 'myjet';
+        divMyjet.className = 'left bottom roundcorner';
+        this.opt.buttons.forEach(function (ibtn, index) {
+            let but = new btn(ibtn);
+            divMyjet.appendChild(but.render());
+        });
+        return divMyjet;
+    }
+}
+
+let myjet = new myJet();
+document.body.appendChild(myjet.render());
